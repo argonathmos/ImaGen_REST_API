fa79e6a8e8e6b3ac3b92799a28125b8c04106f55
Thomas Dargon <ThomasDargon13@gmail.com>
2020-12-09 09:29:14 +0000

    Working and tested on localhost - need to be modified for deployment

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
-const url = require('url');
+const bodyParser = require('body-parser')
 const pixabayCtrl = require('./controllers/pixabayCtrl');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use((req, res, next)=> {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method ==='OPTIONS'){
+        res.header('Access-Control-Allow-Method', 'GET'); 
+        return res.status(200).json({});
+    }
+    next();
+})
 
 app.get('/pixabay', pixabayCtrl.search_get);
 
--- controllers/pixabayCtrl.js
+++ controllers/pixabayCtrl.js
@@ -1,13 +1,31 @@
 const url = require('url');
+const axios = require('axios');
 
 exports.search_get = (req, res, next)=> {
     reqURL = url.parse(req.url, true);
-    console.log(typeof(reqURL));
-    console.log(reqURL);
-    console.log(typeof(reqURL.search));
-    console.log(reqURL.search);
+    // console.log(typeof(reqURL));
+    // console.log(reqURL);
+    // console.log(typeof(reqURL.search));
+    // console.log(reqURL.search);
+    // console.log(typeof(reqURL.query));
+    const searchTerm = reqURL.query.q;
     
-    console.log(typeof(reqURL.query));
-    console.log(reqURL.query.q);
-    res.json({msg: 'My REST API is on fire!'});
+    axios.get('https://pixabay.com/api/', {
+        params:{
+            key: process.env.PIXABAY_API_KEY,
+            q: searchTerm,
+            image_type: 'photo'
+        }
+    })
+    .then(response => {
+        console.log(response);
+        randomHit = Math.floor(Math.random() * Math.floor(response.data.hits.length));
+        res.status(200).json({src: response.data.hits[randomHit].webformatURL});
+        // Never send the response object to the browser as it contains the api key only send back the response.data.
+    })
+    .catch(error => {
+        next(error)
+    })
+    
 }
+
